ff446c5575282872e8b1c1b434c65dad9f38a234
SachinSilva99 <sachinvalo99@gmail.com>
2023-05-21 17:59:08 +0530

    completed homa page :)

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,42 @@
+import Nav from "../Nav/Nav";
+import "./Home.css";
+
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    const listItems = ["Home", "Ads", "Profile", "Withdraw", "Logout"];
+
+    return (
+        <div>
+            <Nav logo={"Obapan.Lk"} listItems={listItems} />
+            <div className="home_content">
+                <div className="home_img"></div>
+                <div className="home-content-left">
+                    <h1 className="heading">
+                        Unlock the power of
+                        <br /> your clicks,
+                        <br /> get paid!
+                    </h1>
+                    <p className="home-about">
+                        Welcome to our click-to-paid website, where your every
+                        click holds the power to earn you money! Unlock a world
+                        of opportunities as you engage with online content,
+                        advertisements, and promotions. Whether you're browsing,
+                        exploring, or simply enjoying your favorite websites, we
+                        believe in rewarding your active participation. With our
+                        innovative platform, you can transform your clicks into
+                        a steady stream of income, making your online experience
+                        more rewarding than ever before. Join the click-to-paid
+                        revolution and start maximizing your earning potential
+                        today!
+                    </p>
+                </div>
+
+            </div>
+            <div className="home_ad"></div>
+            <footer className="home_footer">
+                Copyright © 2023 Obapan.lk. All rights reserved
+            </footer>
+        </div>
+    );
+};
 
 export default Home;
